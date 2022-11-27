9c6cbeda96b45f5620d40f2fa1fc6e5e9d927384
maicol <maykolandres4@gmail.com>
2022-11-27 12:55:48 -0500

    home page done

--- tuto/components/Navbar.jsx
+++ tuto/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
+
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">TUTO</Link>
+      </p>
+
+      <button type='button' className='cart-icon'>
+         <AiOutlineShopping/>
+         <span className='cart-item-qty'>3</span>
+      </button>
+    </div>
   )
 }
 
