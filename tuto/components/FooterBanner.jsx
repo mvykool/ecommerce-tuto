29f41824ba372e11386e8810db19856ae7d4c409
maicol <maykolandres4@gmail.com>
2022-11-27 12:35:23 -0500

    added products, and created layout

--- tuto/components/FooterBanner.jsx
+++ tuto/components/FooterBanner.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Link from 'next/link'
 
-const FooterBanner = () => {
+import { urlFor } from '../lib/client'
+
+const FooterBanner = ({ footerBanner: {discount, largeText, saleTime, samllText, midText, desc, product, buttonText, image}}) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+         <p>{discount}</p>
+         <h3>{largeText}</h3>
+         <p>{saleTime}</p>
+        </div>
+        <div className='right'>
+          <p>{samllText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type='button'>
+              {buttonText}
+            </button>
+          </Link>
+        </div>
+        <img
+        src={urlFor(image)}
+        className="footer-banner-image"  
+        />
+      </div>
+    </div>
   )
 }
 
